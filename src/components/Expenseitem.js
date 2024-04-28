3c77a9368d45b05f9177f3d3779da310747ec98a
pritamawatade <pritamawatade5@gmail.com>
2024-04-28 20:51:15 +0530

    Added a one more component

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,14 +1,15 @@
 import "./Expenseitem.css";
 import ExpenseDate from "./Expensedate.js";
+import Card from "./Card.js";
 function Expenseitem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className=" expense-item__description">
         <h2>{props.title}</h2>
         <div className=" expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import "./Expenses.css";
 import Expenseitem from "./Expenseitem";
+import Card from './Card';
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <Expenseitem
         date={props.item[0].date}
         title={props.item[0].title}
@@ -24,8 +25,13 @@ function Expenses(props) {
         title={props.item[3].title}
         amount={props.item[3].amount} />
   
-    </div>
+    </Card>
   );
 }
 
 export default Expenses;
+
+
+
+
+
