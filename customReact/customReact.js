32918402c7f4fc727241adf7d4c0bbceb478464b
pritamawatade <pritamawatade5@gmail.com>
2024-07-22 19:17:55 +0530

    Build custom react

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -12,7 +12,7 @@ function customRender(reactElement, mainContainer)
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
 
-    for(const prop in reactElement.props)
+    for(const  prop in reactElement.props)
         {
             domElement.setAttribute(prop, reactElement.props[prop])
         }
